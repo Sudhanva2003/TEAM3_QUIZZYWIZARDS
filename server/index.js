7285e4a8c70aa58cca93413ebb816ee4156cde1e
REY <107679192+Shreyas-R-7925@users.noreply.github.com>
2023-11-13 10:44:32 +0530

    server + db files

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const userRoutes = require('./routes/users');
+const quizRoutes = require('./routes/quizzes');
+require('dotenv').config();
+
+console.log("I am functioning");
+
+const app = express();
+const PORT = process.env.PORT || 9000;
+let server;
+
+// middlware config
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: 1, limit: '20mb' }));
+app.use(bodyParser.json({ limit: '20mb' }));
+
+app.use('/api/users/', userRoutes);
+app.use('/api/quizzes/', quizRoutes);
+
+mongoose.connect(process.env.DB_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    }).then(() => console.log('Database connection established'))
+    .catch(er => console.log('Error connecting to mongodb instance: ', er));
+
+server = app.listen(PORT, () => {
+    console.log(`Node server running on port: ${PORT}`);
+});
